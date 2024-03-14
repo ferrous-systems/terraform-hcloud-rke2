6cc84038b76564523b86f3679c5d41975ee2d6d2
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-03-14 11:39:52 -0400

    Do not apply the firewall for now: interferes with cert-manager

--- cluster/firewall.tf
+++ cluster/firewall.tf
@@ -1,15 +1,15 @@
-resource "hcloud_firewall" "cluster" {
-    name   = var.cluster_name
-    labels = {
-        cluster = var.cluster_name
-    }
-    rule {
-        direction  = "in"
-        protocol   = "tcp"
-        port       = "22"
-        source_ips = ["0.0.0.0/0", "::/0"]
-    }
-    apply_to {
-        label_selector = "cluster=${var.cluster_name}"
-    }
-}
+#resource "hcloud_firewall" "cluster" {
+#    name   = var.cluster_name
+#    labels = {
+#        cluster = var.cluster_name
+#    }
+#    rule {
+#        direction  = "in"
+#        protocol   = "tcp"
+#        port       = "22"
+#        source_ips = ["0.0.0.0/0", "::/0"]
+#    }
+#    apply_to {
+#        label_selector = "cluster=${var.cluster_name}"
+#    }
+#}
