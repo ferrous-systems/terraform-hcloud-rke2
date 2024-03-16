46b9dbde0d3c47681fc1a0c0782fff462d9334e3
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-03-16 18:48:46 -0400

    Apply firewall rules

--- modules/hcloud_rke2/firewall.tf
+++ modules/hcloud_rke2/firewall.tf
@@ -1,15 +1,27 @@
-#resource "hcloud_firewall" "cluster" {
-#    name   = var.name
-#    labels = {
-#        cluster = var.name
-#    }
-#    rule {
-#        direction  = "in"
-#        protocol   = "tcp"
-#        port       = "22"
-#        source_ips = ["0.0.0.0/0", "::/0"]
-#    }
-#    apply_to {
-#        label_selector = "cluster=${var.name}"
-#    }
-#}
+resource "hcloud_firewall" "cluster" {
+    name   = var.name
+    labels = {
+        cluster = var.name
+    }
+    rule {
+        direction  = "in"
+        protocol   = "tcp"
+        port       = "80"
+        source_ips = ["0.0.0.0/0", "::/0"]
+    }
+    rule {
+        direction  = "in"
+        protocol   = "tcp"
+        port       = "443"
+        source_ips = ["0.0.0.0/0", "::/0"]
+    }
+    rule {
+        direction  = "in"
+        protocol   = "tcp"
+        port       = "22"
+        source_ips = ["0.0.0.0/0", "::/0"]
+    }
+    apply_to {
+        label_selector = "cluster=${var.name}"
+    }
+}
