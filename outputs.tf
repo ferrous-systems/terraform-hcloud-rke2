b3cbf3f5129f4bced5b771a689893811c6b043c4
Felix Gilcher <felix.gilcher@asquera.de>
2025-10-09 14:11:25 +0200

    expose the indivudual bits of the kubeconfig
    this allows other modules to reuse them to configure their own kubernetes/helm providers just as the module itself does

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,21 @@ output "api_url" {
   value = local.api_url
 }
 
+output "cluster_ca_certificate" {
+  value     = module.cluster.cluster_ca_certificate
+  sensitive = true
+}
+
+output "client_certificate" {
+  value     = module.cluster.client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = module.cluster.client_key
+  sensitive = true
+}
+
 output "lb_ipv4" {
   value = module.cluster.lb_ipv4
 }
